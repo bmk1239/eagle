5d50b187d7b5adb6feb87b7517fbf479daad8c9f
bmk1239 <bmk1239@gmail.com>
2025-06-20 23:56:43 +0300

    Update script.py

--- script.py
+++ script.py
@@ -47,7 +47,7 @@ params = {
     "EnableImages": "false",
 }
 headers = {
-    "X-Emby-Token": a9a768bda323427ea639cb6277d736bb,
+    "X-Emby-Token": "a9a768bda323427ea639cb6277d736bb",
     #"X-Emby-Client": "Emby Web",
     #"X-Emby-Device-Name": "Google Chrome Android",
     #"X-Emby-Device-Id": DEVICE_ID,
