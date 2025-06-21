cc76225639ee192c44abfbbc0eb894c7e1471dd0
bmk1239 <bmk1239@gmail.com>
2025-06-21 16:20:15 +0300

    Update script1.py

--- script1.py
+++ script1.py
@@ -1,6 +1,7 @@
 import gzip
 import requests
 import xml.etree.ElementTree as ET
+from xml.dom import minidom
 
 URLS = [
     "https://epgshare01.online/epgshare01/epg_ripper_IL1.xml.gz",
@@ -10,34 +11,41 @@ URLS = [
 def download_and_parse(url):
     response = requests.get(url)
     response.raise_for_status()
-    return ET.fromstring(gzip.decompress(response.content).decode('utf-8'))
+    xml_data = gzip.decompress(response.content).decode("utf-8")
+    return ET.fromstring(xml_data)
 
 def main():
     seen_ids = set()
-    part_a = []
-    part_b = []
+    part_a = []  # <channel>
+    part_b = []  # <programme>
 
     for url in URLS:
         root = download_and_parse(url)
 
-        # Collect "channel" elements (but avoid using that word in variables)
-        for el in root.findall("channel"):
-            el_id = el.attrib.get("id")
-            if el_id and el_id not in seen_ids:
-                seen_ids.add(el_id)
-                part_a.append(el)
+        for el in root:
+            if el.tag == "channel":
+                cid = el.attrib.get("id")
+                if cid and cid not in seen_ids:
+                    seen_ids.add(cid)
+                    part_a.append(el)
+            elif el.tag == "programme":
+                part_b.append(el)
 
-        # Collect all "programme" elements
-        part_b.extend(root.findall("programme"))
-
-    # Create the root of the new EPG XML
-    out_root = ET.Element("tv")
+    # Build merged XML
+    root_out = ET.Element("tv")
     for el in part_a:
-        out_root.append(el)
+        root_out.append(el)
     for el in part_b:
-        out_root.append(el)
+        root_out.append(el)
 
-    ET.ElementTree(out_root).write("file1.xml", encoding="utf-8", xml_declaration=True)
+    # Convert to string and pretty-print with minidom
+    raw_str = ET.tostring(root_out, encoding="utf-8")
+    parsed = minidom.parseString(raw_str)
+    pretty_xml = parsed.toprettyxml(indent="  ", encoding="utf-8")
+
+    # Write final output
+    with open("file1.xml", "wb") as f:
+        f.write(pretty_xml)
 
 if __name__ == "__main__":
     main()
