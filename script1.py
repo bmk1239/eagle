2f643e988e188d4bd39a70f7099b0fe4ee0c6849
bmk1239 <bmk1239@gmail.com>
2025-06-21 16:14:19 +0300

    Update script1.py

--- script1.py
+++ script1.py
@@ -10,32 +10,34 @@ URLS = [
 def download_and_parse(url):
     response = requests.get(url)
     response.raise_for_status()
-    xml_content = gzip.decompress(response.content).decode('utf-8')
-    return ET.fromstring(xml_content)
+    return ET.fromstring(gzip.decompress(response.content).decode('utf-8'))
 
 def main():
     seen_ids = set()
-    group_a = []
-    group_b = []
+    part_a = []
+    part_b = []
 
     for url in URLS:
         root = download_and_parse(url)
 
+        # Collect "channel" elements (but avoid using that word in variables)
         for el in root.findall("channel"):
             el_id = el.attrib.get("id")
             if el_id and el_id not in seen_ids:
                 seen_ids.add(el_id)
-                group_a.append(el)
+                part_a.append(el)
 
-        group_b.extend(root.findall("programme"))
+        # Collect all "programme" elements
+        part_b.extend(root.findall("programme"))
 
-    output_root = ET.Element("tv")
-    for el in group_a:
-        output_root.append(el)
-    for el in group_b:
-        output_root.append(el)
+    # Create the root of the new EPG XML
+    out_root = ET.Element("tv")
+    for el in part_a:
+        out_root.append(el)
+    for el in part_b:
+        out_root.append(el)
 
-    ET.ElementTree(output_root).write("file1.xml", encoding="utf-8", xml_declaration=True)
+    ET.ElementTree(out_root).write("file1.xml", encoding="utf-8", xml_declaration=True)
 
 if __name__ == "__main__":
     main()
