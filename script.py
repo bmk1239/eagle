15305178189b9bcc6498eb98a63d3ff1ca5e592d
bmk1239 <bmk1239@gmail.com>
2025-06-21 14:39:56 +0300

    Update script.py

--- script.py
+++ script.py
@@ -86,7 +86,7 @@ response = requests.get(url, headers=headers, params=params)
 info = response.json().get("Items", []) 
 
 # Count check
-current_count = len(info) - 1
+current_count = len(info)
 try:
     previous_count = int(COUNT_FILE.read_text().strip())
 except Exception:
